8397ad804b866221dc51ccc51d7198236dbef9a1
DiegoNavarro <diegonava1023@gmail.com>
2026-05-06 13:35:08 -0500

    Se agrega el toast,browser y comentarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import HotelList from './components/HotelList'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HotelList from './components/HotelList';
+import { Toaster } from 'react-hot-toast';
+import HotelCreate from './components/HotelCreate';
+import RoomConfig from './components/RoomConfig';
 function App() {
-
-  //return del componente HotelList
   return (
-    <div className='container mx-auto'>
-      <HotelList />
-    </div>
+    //Componente BrowserRouter para el manejo de rutas
+    <BrowserRouter>
+      //Componente Toaster para notificaciones
+      <Toaster position="top-right" reverseOrder={false} />{
+
+      }
+      //Componente Container para manejo de la pantalla
+      <div className='container mx-auto p-4'>
+        <Routes>
+          <Route path="/" element={<HotelList />} />
+          <Route path="/nuevo-hotel" element={<HotelCreate />} />
+          <Route path="/hoteles/:id/habitaciones" element={<RoomConfig />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
